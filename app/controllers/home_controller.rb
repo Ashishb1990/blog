8786df2534e312c50dc28b48eb21c84fbc362fe2
= <=>
2017-03-03 19:15:10 +0530

    changes done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
+  
   def index
   	if params[:search].present?
   	  @blogs = Blog.where(" title LIKE ? OR description LIKE ?", params[:search],params[:search])
-  
-      elsif params[:start_date] && params[:start_date]
-      @blogs =Blog.where("created_at >= :start_date AND created_at <= :end_date", {start_date: params[:start_date].to_time, end_date: params[:end_date].to_time}
+    # elsif params[:start_date] && params[:start_date]
+    #   @blogs =Blog.where("created_at >= :start_date AND created_at <= :end_date", {start_date: params[:start_date].to_time, end_date: params[:end_date].to_time}
        else
   	  @blogs = Blog.all
   	end
